dc45de2a6b469c1b1e23c41f9431e6951177ff5b
MaiqTheHonest <rdemka@gmail.com>
2025-02-13 23:51:18 +0000

    fix weekend bug

--- rust_version/src/stats.rs
+++ rust_version/src/stats.rs
@@ -10,9 +10,13 @@ pub fn calculate_returns(
     let mut return_history: HashMap<NaiveDate, f64> = HashMap::new();
     let mut portfolio = portfolio_history[0].1.clone();
     // println!("{:?}", portfolio_history);
-    let mut q_total: f64 = 0.0;
-    let mut sum_of_mid_returns: f64 = 0.0;
+    let mut outer_holder_value: f64 = 0.0;
+    let mut outer_holder_sum: f64 = 0.0;
+
     for tupleobject in portfolio_history {
+
+        let mut value_total: f64 = 0.0;
+        let mut sum_of_mid_returns: f64 = 0.0;
 
         let date: NaiveDate = tupleobject.0;
 
@@ -22,23 +26,27 @@ pub fn calculate_returns(
           // pass, portfolio remains same as 1 day (iteration) before  
         }
 
-
+        // if found, value is value and mid return is mid return
         
-        for (ticker, (q_0, p_0)) in portfolio.clone().into_iter() {
-            let single_history = complete_prices.get(&ticker)?;
+        for (ticker, (q_0, p_0)) in &portfolio {
+            let single_history = complete_prices.get(ticker)?;
             // println!("{:?}", single_history);
             if let Some(v) = single_history.get(&date) {        // if price_history doesn't contain this date, it was a weekend.
                 let p_1: f64 = *v;
-                let mid_return: f64 = q_0*(p_1/p_0 - 1.0);
-                q_total += q_0;
+                let mid_return: f64 = p_0*q_0*(p_1/p_0 - 1.0);
+                value_total += q_0*p_0;
                 sum_of_mid_returns += mid_return;
-            } else {break};
+                outer_holder_value = value_total;
+                outer_holder_sum = sum_of_mid_returns;
+            } else {value_total = outer_holder_value.clone();
+                    sum_of_mid_returns = outer_holder_sum.clone();
+                    };
 
         };
 
-        let daily_return = (100.0/q_total)*sum_of_mid_returns;
-        println!("{:?}", daily_return);
+        let daily_return = (100.0/value_total)*sum_of_mid_returns;
+        println!("{:?}, {:?}", date, daily_return);
         return_history.insert(date, daily_return);    
     };
     Some(return_history)
-}// if could get it, do as normal. if couldn't get it, last line + break
+} // if could get it, do as normal. if couldn't get it, last line + break
